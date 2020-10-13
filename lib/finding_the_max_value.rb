e5ca4d8a426bd5eebcbe12848c2a8ac8606b6b51
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-13 22:14:49 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,6 +1,6 @@
 def find_max_value(array)
   count = 0 
-  largest_number = 0 
+  largest_number = array[count] 
   while array[count] < array.length 
     if array[count] > largest_number
       largest_number = array[count]
